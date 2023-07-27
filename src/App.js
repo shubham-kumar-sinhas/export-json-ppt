045f7885bf9da945b605a9e2a11b852d8bdf2afb
shubham-kumar-sinhas <shubhamsinha3110@gmail.com>
2023-07-27 14:38:02 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Downloadppt from './Components/ppt/Downloadppt';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Downloadppt/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
